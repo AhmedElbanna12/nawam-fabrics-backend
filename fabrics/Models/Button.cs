56fa84e4f09f9632809de9afcce19d0c82c0d4c0
AhmedElbanna12 <ahmedalbanna763@gmail.com>
2025-10-10 02:51:05 +0300

    edit photos

--- fabrics/Models/Button.cs
+++ fabrics/Models/Button.cs
@@ -1,9 +1,22 @@
-﻿namespace fabrics.Models
+﻿using System.Text.Json.Serialization;
+
+namespace fabrics.Models
 {
     public class Button
     {
+
+
+
+        [JsonPropertyName("type")]
         public string Type { get; set; } = "postback";
+
+        [JsonPropertyName("title")]
         public string Title { get; set; }
+
+
+
+        [JsonPropertyName("payload")]
+
         public string Payload { get; set; }
     }
 }
--- fabrics/Models/GenericTemplateElement.cs
+++ fabrics/Models/GenericTemplateElement.cs
@@ -1,10 +1,29 @@
-﻿namespace fabrics.Models
+﻿using System.Text.Json.Serialization;
+
+namespace fabrics.Models
 {
     public class GenericTemplateElement
     {
+
+
+
+
+        [JsonPropertyName("title")]
+
         public string Title { get; set; }
+
+
+
+        [JsonPropertyName("subtitle")]
         public string Subtitle { get; set; }
-        public string ImageUrl { get; set; }
+
+
+
+        [JsonPropertyName("image_url")]
+        public string image_url { get; set; }
+
+
+        [JsonPropertyName("buttons")]
         public List<Button> Buttons { get; set; } = new List<Button>();
     }
 }
